272928d0e3e76cde4bb367ce2a9d51815737863b
Dmitry Nazarov <dmitriy.nazarov@brymonsoft.com>
2021-02-15 23:15:57 +0300

    port fix

--- server.py
+++ server.py
@@ -1,21 +1,20 @@
 import json
 import os
-import sys
 
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/get_data', methods=["POST", "GET"])
-def ready():
+def get_data():
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    filename = str(dir_path)+"/gomel.json"
-    # data = request.get_json()
+    filename = str(dir_path) + "/gomel.json"
     f = open(filename)
     coordinates = json.load(f)
     f.close()
     print("awdawd")
     return jsonify(coordinates)
 
-app.run(host="0.0.0.0")
 
+app.run(host="0.0.0.0", port=os.environ["PORT"])
