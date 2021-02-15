728ef2d217676c0d3ed502a7b64ea1b7a918b471
Dmitry Nazarov <dmitriy.nazarov@brymonsoft.com>
2021-02-15 22:47:58 +0300

    json fix

--- server.py
+++ server.py
@@ -8,12 +8,14 @@ app = Flask(__name__)
 
 @app.route('/get_data', methods=["POST", "GET"])
 def ready():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    filename = str(dir_path)+"/gomel.json"
     # data = request.get_json()
-    f = open("/gomel.json")
+    f = open(filename)
     coordinates = json.load(f)
     f.close()
     print("awdawd")
     return jsonify(coordinates)
 
-app.run(host="0.0.0.0", port=os.environ["PORT"])
+app.run(host="0.0.0.0")
 
