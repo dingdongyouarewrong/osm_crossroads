79fde3f402f298f1310ff1372483a8478aabf498
Dmitry Nazarov <dmitriy.nazarov@brymonsoft.com>
2021-02-19 23:36:33 +0300

    req by city

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -13,8 +13,18 @@ def get_data():
     f = open(filename)
     coordinates = json.load(f)
     f.close()
-    print("awdawd")
     return jsonify(coordinates)
 
+@app.route('/get_data_by_city', methods=["POST", "GET"])
+def get_data_by_city():
+    city = request.args.get('city')
+    print(city)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    filename = str(dir_path) + "/gomel.json"
+    f = open(filename)
+    coordinates = json.load(f)
+    f.close()
+    print("awdawd")
+    return jsonify(coordinates)
 
 app.run(host="0.0.0.0", port=os.environ["PORT"])
