e0434d656094368a3060ccea078f5f904ce902ad
Dmitry Nazarov <dmitriy.nazarov@brymonsoft.com>
2021-02-21 11:17:02 +0300

    welcome to sCUM

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
 import json
 import os
+import netifaces as nif
 
-from flask import Flask, jsonify, request
+# git push heroku master
+from flask import Flask, jsonify, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='template')
 
+@app.route('/')
+def scum():
+    ip = request.remote_addr
+    print(ip)
+    'Returns a list of MACs for interfaces that have given IP, returns None if not found'
+    for i in nif.interfaces():
+        addrs = nif.ifaddresses(i)
+        # if_mac = addrs[nif.AF_LINK][0]['addr']
+        # if_ip = addrs[nif.AF_INET][0]['addr']
+        print(addrs)
+    return render_template('hello.html')
 
 @app.route('/get_data', methods=["POST", "GET"])
 def get_data():
@@ -27,4 +40,4 @@ def get_data_by_city():
     print("awdawd")
     return jsonify(coordinates)
 
-app.run(host="0.0.0.0", port=os.environ["PORT"])
+app.run(host="0.0.0.0")
